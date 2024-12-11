7e83a1312506844f6b081df6968d9cb76c3e03be
miranda <161040125+mmfforgithub@users.noreply.github.com>
2024-12-11 16:16:46 -0300

    Update models.py

--- models.py
+++ models.py
@@ -31,22 +31,5 @@ class Livro(db.Model):
 
     def __repr__(self):
         return '<Livro {}>'.format(self.titulo)
-    
-class Pedido(db.Model):
-    __tablename__ = 'pedido'
-    id = db.Column(db.Integer, primary_key = True)
-    editora_id = db.Column(db.Integer, db.ForeignKey('editora.id'))
-    livro_id = db.Column(db.Integer, db.ForeignKey('livro.id'))
-    data = db.Column(db.Date)
 
-    editora = db.relationship('Editora', foreign_keys = editora_id)
-    livro = db.relationship('Livro', foreign_keys = livro_id)
-
-    def __init__(self, editora_id, livro_id, data):
-        self.editora_id = editora_id
-        self.livro_id = livro_id
-        self.data = data
-
-    def __repr__(self):
-        return '<Pedido {} - {} - {}>'.format(self.editora.nome, self.livro.titulo, self.data)
     
