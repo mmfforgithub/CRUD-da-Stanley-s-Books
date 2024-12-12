4ee5b5f6d3922bf6132a2bb52f865a62a4e11290
miranda <161040125+mmfforgithub@users.noreply.github.com>
2024-12-12 09:45:20 -0300

    Update editora.py

--- modulos/editora/editora.py
+++ modulos/editora/editora.py
@@ -6,8 +6,8 @@ bp_editora = Blueprint('editora', __name__, template_folder='templates')
 
 @bp_editora.route('/')
 def index():
-    dados = Editora.query.all()
-    return render_template('editora.html', dados=dados)
+    e = Editora.query.all()
+    return render_template('editora.html', dados=e)
 
 @bp_editora.route('/add')
 def add():
@@ -26,3 +26,36 @@ def save():
     else:
         flash('Preencha todos os campos!')
         return redirect('/editora/add')
+
+@bp_editora.route("/remove/<int:id>")
+def remove(id):
+    e = Editora.query.get(id)
+    try:
+        db.session.delete(e)
+        db.session.commit()
+        flash("Editora removida!")
+    except:
+        flash("Editora Inválida!")
+    return redirect("/editora")
+
+
+@bp_editora.route("/edit/<int:id>")
+def edit(id):
+    e = Editora.query.get(id)
+    return render_template("editora_edit.html", dados=e)
+
+
+@bp_editora.route("/edit-save", methods=['POST'])
+def edit_save():
+    nome = request.form.get("nome")
+    fundacao = request.form.get("fundacao")
+    id = request.form.get("id")
+    if nome and fundacao and id:
+        e = Editora.query.get(id)
+        e.nome = nome
+        e.fundacao = fundacao
+        db.session.commit()
+        flash("Dados atualizados!")
+    else:
+        flash("Preencha todos os campos!")
+    return redirect("/editora")
